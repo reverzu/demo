cbd45a2963a79deb33138958337edf52aa201a09
Abhijeet Khatri <76448535+reverzu@users.noreply.github.com>
2024-02-06 19:32:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import cv2
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
+if "run" not in st.session_state:
+    st.session_state["run"] = False
 
 class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
@@ -12,8 +14,12 @@ class VideoTransformer(VideoTransformerBase):
         return img
 
 
-
-button = st.button("This is a Button")
-if button:
-  st.text("Clicked")
-  webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
+if st.session_state["run"]:
+    webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
+    
+button_yes = st.button("Start Streaming 📸")
+button_no = st.button("Stop Streaming 📷")
+if button_yes:
+  st.session_state["run"] = True
+if button_no:
+  st.session_state["run"] = False
