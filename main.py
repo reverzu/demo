f282eb3ce1c6b556de6c7124fb9e911b8ac52223
Abhijeet Khatri <76448535+reverzu@users.noreply.github.com>
2024-02-06 19:29:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import cv2
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+
+
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+        return img
+
+
 
 button = st.button("This is a Button")
 if button:
   st.text("Clicked")
-  webrtc_streamer(key="example")
+  webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
